cb4c68c8b3a5ec1dc5281324ef38e98a5b1384ae
rybhrdwj-tn <ricky.bhardwaj@techolution.com>
2025-11-05 16:40:38 +0530

    make meet bot headless

--- services/meetBot.js
+++ services/meetBot.js
@@ -30,7 +30,7 @@ async function ensureAuthSession(meetingUrl, asGuest = false) {
   console.log("🔐 Ensuring authentication session...");
 
   browser = await chromium.launch({
-    headless: false,
+    headless: true,
     args: [
       "--disable-blink-features=AutomationControlled",
       "--start-maximized",
